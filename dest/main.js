44013606b7fcf37a7d8956d8d0f3876a13848427
Vu Thanh Trung <Trungvu027@gmail.com>
2021-07-19 22:00:36 +0700

    layout

--- dest/main.js
+++ dest/main.js
@@ -4,7 +4,7 @@ let cursorfollow = document.getElementById("cursorfollow");
 window.addEventListener("mousemove", function(e) {
     let x = e.clientX; // lấy giá trị tọa độ x
     let y = e.clientY; // lấy giá trị tọa độ Y
-
+    console.log(e);
     cursor.style.left = x + "px"; // gán x cho cursor
     cursor.style.top = y + "px"; // gán y cho cursor
 });
