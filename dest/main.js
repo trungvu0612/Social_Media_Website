09ffff82b300faa9de1531b934f6bfca5b99de45
Vu Thanh Trung <Trungvu027@gmail.com>
2021-07-19 17:13:02 +0700

    cursor mouse move

--- dest/main.js
+++ dest/main.js
@@ -1,4 +1,10 @@
-let cursor = document.querySelectorAll(".cursor");
-let cursorfollow = document.querySelectorAll(".cursorfollow");
+let cursor = document.getElementById("cursor");
+let cursorfollow = document.getElementById("cursorfollow");
 
-window.addEventListener("mousemove", function(e) {});
+window.addEventListener("mousemove", function(e) {
+    let x = e.clientX; // lấy giá trị tọa độ x
+    let y = e.clientY; // lấy giá trị tọa độ Y
+
+    cursor.style.left = x + "px"; // gán x cho cursor
+    cursor.style.top = y + "px"; // gán y cho cursor
+});
