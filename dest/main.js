ccb50288ed24abd8c457f816047977974826866c
Vu Thanh Trung <Trungvu027@gmail.com>
2021-07-20 11:51:19 +0700

    about section

--- dest/main.js
+++ dest/main.js
@@ -1,10 +1,12 @@
-let cursor = document.getElementById("cursor");
-let cursorfollow = document.getElementById("cursorfollow");
+// -- cursor move
 
-window.addEventListener("mousemove", function(e) {
-    let x = e.clientX; // lấy giá trị tọa độ x
-    let y = e.clientY; // lấy giá trị tọa độ Y
-    console.log(e);
-    cursor.style.left = x + "px"; // gán x cho cursor
-    cursor.style.top = y + "px"; // gán y cho cursor
-});
+// let cursor = document.getElementById("cursor");
+// let cursorfollow = document.getElementById("cursorfollow");
+
+// window.addEventListener("mousemove", function(e) {
+//     let x = e.clientX; // lấy giá trị tọa độ x
+//     let y = e.clientY; // lấy giá trị tọa độ Y
+//     console.log(e);
+//     cursor.style.left = x + "px"; // gán x cho cursor
+//     cursor.style.top = y + "px"; // gán y cho cursor
+// });
