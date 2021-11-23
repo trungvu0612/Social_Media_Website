90f4e097862a4a4e1f0a53f89e026eec6d87ed10
Vu Thanh Trung <Trungvu027@gmail.com>
2021-11-23 21:10:26 +0700

    popup create at create page

--- Frontend/src/component/categorymusic/listmusic.jsx
+++ Frontend/src/component/categorymusic/listmusic.jsx
@@ -4,13 +4,20 @@ import { apiUploadFileMp3, apiUploadImgMp3 } from "../../contexts/constants";
 export default function ListMusic({
   music: { musicName, musicAuthor, musicImg, musicFile },
 }) {
+  function createPost() {
+    const popupCreate = document.querySelector(".create-popup");
+    popupCreate.classList.add("active");
+  }
+
   return (
     <div className="like">
       <img className="img" src={`${apiUploadImgMp3}${musicImg}`} alt="" />
       <h5 className="name">{musicName}</h5>
       <h5 className="author">{musicAuthor}</h5>
       <audio className="audio" src={`${apiUploadFileMp3}${musicFile}`} />
-      <button className="item-create">Create post</button>
+      <button className="item-create" onClick={createPost}>
+        Create post
+      </button>
       <button className="item-create">Download</button>
     </div>
   );
--- Frontend/src/page/home/component/postCreate.jsx
+++ Frontend/src/page/home/component/postCreate.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CategoryMusic from "../../../component/categoryMusic";
+import PopupCreate from "./popupCreate";
 
 export default function PostCreate() {
   return (
     <div className="postcreate post-list post-list-4">
       <CategoryMusic />
+      <PopupCreate />
     </div>
   );
 }
